93e9a31a979842f5ee22af94d8f978aa795773db
Michał Śnieżko <michal.sniezko@codibly.com>
2023-10-20 13:44:56 +0200

    custom commands

--- cypress/e2e/testWithPageObjects.js
+++ cypress/e2e/testWithPageObjects.js
@@ -5,7 +5,7 @@ const { onSmartTablePage } = require("../support/page_objects/smartTablePage")
 
 describe('Test with Page Objects', () => {
     beforeEach('open application', () => {
-        cy.visit('/')
+        cy.openHomePage()
     })
 
     it('verify navigation across the pages', () => {
